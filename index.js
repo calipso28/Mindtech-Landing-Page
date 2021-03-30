d71df399d4a78b80d5354a251d9d9b767e35ff3a
calipso28 <71214660+calipso28@users.noreply.github.com>
2021-03-30 23:10:34 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,18 @@
+// Mobile View - Menu Class Switch
+// Jquery Menu Settings
+
+var main = function() {
+
+    // Menu Container show Function
+    
+    $('.hamburger').on('click',() => {
+        $('.big').fadeToggle(".show");
+        $('.nav-ul').toggleClass("show");
+        $('.nav-a').toggleClass("show");   
+        $('#mobileicon').toggleClass("show");    
+    })
+    
+      
+    };
+    
+    $(document).ready(main);
